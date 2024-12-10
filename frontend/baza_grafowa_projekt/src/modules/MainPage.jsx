3ca4dcd42e6ecb667a4ba51c9bb550d8e988a426
jakar02 <jakub.mateusz.karas@gmail.com>
2024-12-10 20:55:42 +0100

    working app

--- frontend/baza_grafowa_projekt/src/modules/MainPage.jsx
+++ frontend/baza_grafowa_projekt/src/modules/MainPage.jsx
@@ -12,17 +12,25 @@ import {
   Box,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add"; // Import ikony plusa
+import EditIcon from "@mui/icons-material/Edit";
 
 function MainPage() {
+  const [updating, setUpdating] = useState(false);
   const [familyTrees, setFamilyTrees] = useState([]);
   const [selectedTree, setSelectedTree] = useState(null);
   const [editingTree, setEditingTree] = useState(null); // Stan dla nowego drzewa
-
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [birthYear, setBirthYear] = useState("");
+  const [deathYear, setDeathYear] = useState("");
   const [founderAdded, setFounderAdded] = useState(false);
   const [ifSaveFamily, setIfSaveFamily] = useState(false);
-  // Funkcja do obsługi kliknięcia drzewa
+  const [expandedNode, setExpandedNode] = useState(null); // Jeden rozwinięty węzeł
+
+  const handleExpandNode = (personId) => {
+    setExpandedNode(expandedNode === personId ? null : personId); // Tylko jeden węzeł może być rozwinięty
+  };
+
   const handleTreeSelect = (tree) => {
     setSelectedTree(tree);
     setEditingTree(null); // Wyłącz edycję nowego drzewa
@@ -31,72 +39,68 @@ function MainPage() {
   const handleAddFamilyTree = () => {
     const newTree = {
       treeName: "New Family Tree",
-      firstName: "",
-      lastName: "",
-      partners: [],
+      name: "",
+      surname: "",
+      partners: null,
       children: [],
+      birthYear: "",
+      deathYear: "",
     };
     setEditingTree(newTree);
+    setUpdating(false);
+    setFounderAdded(false);
   };
 
   // Funkcja do dodania nowego członka do drzewa
   const handleAddPartner = (parent) => {
-    if (firstName.trim()) {
-      //const updatedTree = { ...editingTree };
-      
-      if(parent.partners.length > 0){
-        parent.partners[0] = {
-          firstName: firstName,
-          lastName: lastName,
-          partners: [],
+    if (name.trim()) {
+      if (parent.partners == null) {
+        parent.partners = {
+          name: name,
+          surname: surname,
+          partners: null,
           children: [],
+          birthYear: birthYear,
+          deathYear: deathYear,
         };
       }
-      else{
-        parent.partners.push({
-          firstName: firstName,
-          lastName: lastName,
-          partners: [],
-          children: [],
-        });
-      }
-
 
       const updatedTree = { ...editingTree };
-      console.log("Dodano partnera:", parent);
 
       setEditingTree(updatedTree);
-      setFirstName(""); // Reset pola
-      setLastName(""); // Reset pola
-      
+      setName(""); // Reset pola
+      setSurname(""); // Reset pola
+      setBirthYear("");
+      setDeathYear("");
+      setExpandedNode(null); // Zwijanie po dodaniu
     }
   };
 
-  const handleAddFounder = () => {
-    setFounderAdded(true);
-  };
-
   const handleAddChild = (parent) => {
-    if (firstName.trim()) {
+    if (name.trim()) {
       const updatedTree = { ...editingTree };
       parent.children.push({
-        firstName: firstName,
-        lastName: lastName,
-        partners: [],
+        name: name,
+        surname: surname,
+        partners: null,
         children: [],
+        birthYear: birthYear,
+        deathYear: deathYear,
       });
       setEditingTree(updatedTree);
-      setFirstName(""); // Reset pola
-      setLastName(""); // Reset pola
+      setName(""); // Reset pola
+      setSurname(""); // Reset pola
+      setBirthYear("");
+      setDeathYear("");
+      setExpandedNode(null); // Zwijanie po dodaniu
     }
   };
 
   // Funkcja do zapisywania drzewa genealogicznego
   const handleSaveFamilyTree = async () => {
-    
-    setFounderAdded(false);
+    setUpdating(false);
     console.log("Zapisywanie drzewa genealogicznego:", editingTree);
-    if (!editingTree.firstName || !editingTree.lastName) {
+    if (!editingTree.name || !editingTree.surname) {
       alert("Podaj imię i nazwisko założyciela drzewa!");
       return;
     }
@@ -107,19 +111,64 @@ function MainPage() {
         "http://localhost:8080/api/persons/add-family-tree",
         {
           treeName: editingTree.treeName,
-          name: editingTree.firstName,
-          surname: editingTree.lastName,
+          name: editingTree.name,
+          surname: editingTree.surname,
           partners: editingTree.partners,
           children: editingTree.children,
+          birthYear: parseInt(editingTree.birthYear, 10),
+          deathYear: parseInt(editingTree.deathYear, 10),
         }
       );
 
       // Jeśli odpowiedź jest pozytywna, dodajemy drzewo do lokalnego stanu
       if (response.status === 200) {
+        setFounderAdded(false);
         setFamilyTrees([...familyTrees, response.data]); // Zakładając, że odpowiedź zawiera zapisane drzewo
         alert("Drzewo genealogiczne zostało zapisane!");
       } else {
         alert("Błąd podczas zapisywania drzewa genealogicznego.");
+      }
+
+      // Resetujemy stan edytowanego drzewa po zapisaniu
+      setEditingTree(null);
+      setIfSaveFamily(!ifSaveFamily);
+    } catch (error) {
+      console.error("Błąd zapisywania drzewa:", error);
+      alert("Wystąpił błąd podczas zapisywania drzewa. Spróbuj ponownie.");
+    }
+  };
+
+  const handleUpdateFamilyTree = async () => {
+    console.log("Aktualizowanie drzewa genealogicznego:", editingTree);
+    if (!editingTree.name || !editingTree.surname) {
+      alert("Podaj imię i nazwisko założyciela drzewa!");
+      return;
+    }
+
+    try {
+      // Wykonanie żądania POST do API, aby zapisać drzewo genealogiczne
+      const response = await axios.post(
+        "http://localhost:8080/api/persons/edit-family-tree",
+        {
+          id: editingTree.id,
+          treeName: editingTree.treeName,
+          name: editingTree.name,
+          surname: editingTree.surname,
+          partners: editingTree.partners,
+          children: editingTree.children,
+          birthYear: parseInt(editingTree.birthYear, 10),
+          deathYear: parseInt(editingTree.deathYear, 10),
+        }
+      );
+
+      // Jeśli odpowiedź jest pozytywna, dodajemy drzewo do lokalnego stanu
+      if (response.status === 200) {
+        setFounderAdded(false);
+        setUpdating(false);
+        setFamilyTrees([...familyTrees, response.data]); // Zakładając, że odpowiedź zawiera zapisane drzewo
+        alert("Drzewo genealogiczne zaktualizowane!");
+      } else {
+        alert("Błąd podczas aktualizacji drzewa genealogicznego.");
       }
 
       // Resetujemy stan edytowanego drzewa po zapisaniu
@@ -144,73 +193,144 @@ function MainPage() {
     }
   };
 
+  const handleEditTree = (tree) => {
+    console.log("Edytowanie drzewa:", tree);
+    setEditingTree(tree);
+    setFounderAdded(true);
+    setUpdating(true);
+  };
+
   useEffect(() => {
     getAllTrees();
+    setSelectedTree(familyTrees[0]);
   }, [ifSaveFamily]);
 
-  const renderFamilyTree = (person, level = 0) => (
-    <Box
-      key={person.id}
-      marginLeft={`${level * 30}px`}
-      padding="10px"
-      borderLeft="2px solid #ccc"
-    >
-      {/* Wyświetlanie osoby i jej partnera */}
-      <Typography variant="body1">
-        {person.firstName} {person.lastName}
-        {console.log("person partners: ", person.partners)}
-        {person.partners.length > 0
-          ? " and " + person.partners[0].firstName + " " + person.partners[0].lastName
-          : ""}
-      </Typography>
+  useEffect(() => {
+    if(familyTrees.length > 2)
+    setSelectedTree(familyTrees[2]);
+  }, [familyTrees]);
 
-      {/* Wyświetlanie dzieci */}
-      {person.children.length > 0 && (
-        <Box marginLeft="10px">
-          {person.children.map((child) => renderFamilyTree(child, level + 1))}
-        </Box>
-      )}
+  const renderFamilyTree = (person, level = 0) => {
+    const uniqueKey = `${person.name}-${person.surname}-${level}`;
 
-      {/* Dodawanie partnera i dziecka dla każdego poziomu */}
-      {editingTree && (
-        <Box marginTop="10px">
-          <TextField
-            size="small"
-            sx={{ width: 130 }}
-            label="Name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            style={{ marginRight: "10px" }}
-          />
-          <TextField
-            size="small"
-            sx={{ width: 130 }}
-            label="Surname"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            style={{ marginRight: "10px" }}
-          />
-          <Button
-            startIcon={<AddIcon />}
-            variant="contained"
-            color="primary"
-            onClick={() => handleAddPartner(person)}
-            style={{ marginRight: "10px" }}
-          >
-            Add as a Partner
-          </Button>
-          <Button
-            startIcon={<AddIcon />}
-            variant="contained"
-            color="primary"
-            onClick={() => handleAddChild(person)}
-          >
-            Add as a Child
-          </Button>
-        </Box>
-      )}
-    </Box>
-  );
+    return (
+      <Box
+        key={uniqueKey} // Unikalny klucz węzła
+        marginLeft={`${level * 30}px`} // Wcięcie w zależności od poziomu
+        padding="10px"
+        borderLeft="2px solid #ccc"
+      >
+        <Typography variant="body1">
+          {
+            <>
+              {person ? (
+                <>
+                  <b>
+                    {person.name} {person.surname}
+                  </b>{" "}
+                  ({person.birthYear > 0 ? person.birthYear : ""}-
+                  {person.deathYear > 0 ? person.deathYear : "now"})
+                </>
+              ) : null}
+            </>
+          }
+          {person.partners ? (
+            <>
+              {" and "}
+              <b>
+                {person.partners.name} {person.partners.surname}
+              </b>{" "}
+              ({person.partners.birthYear > 0 ? person.partners.birthYear : ""}-
+              {person.partners.deathYear > 0
+                ? person.partners.deathYear
+                : "now"}
+              )
+            </>
+          ) : (
+            ""
+          )}
+        </Typography>
+
+        {/* Przyciski edycji widoczne tylko dla nowo tworzonych drzew */}
+        {editingTree && (
+          <>
+            {expandedNode === uniqueKey ? ( // Rozwijamy tylko węzeł z unikalnym kluczem
+              <Box marginTop="10px">
+                <TextField
+                  size="small"
+                  sx={{ width: 130 }}
+                  label="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  style={{ marginRight: "10px" }}
+                />
+                <TextField
+                  size="small"
+                  sx={{ width: 130 }}
+                  label="Surname"
+                  value={surname}
+                  onChange={(e) => setSurname(e.target.value)}
+                  style={{ marginRight: "10px" }}
+                />
+                <TextField
+                  size="small"
+                  sx={{ width: 65 }}
+                  label="Birth year"
+                  value={birthYear}
+                  onChange={(e) => setBirthYear(e.target.value)}
+                  style={{ marginRight: "10px" }}
+                />
+                <TextField
+                  size="small"
+                  sx={{ width: 65 }}
+                  label="Death year"
+                  value={deathYear}
+                  onChange={(e) => setDeathYear(e.target.value)}
+                  style={{ marginRight: "10px" }}
+                />
+                <Button
+                  startIcon={<AddIcon />}
+                  variant="contained"
+                  color="primary"
+                  onClick={() => handleAddPartner(person)}
+                  style={{ marginRight: "10px" }}
+                >
+                  Add as a Partner
+                </Button>
+                <Button
+                  startIcon={<AddIcon />}
+                  variant="contained"
+                  color="primary"
+                  onClick={() => handleAddChild(person)}
+                >
+                  Add as a Child
+                </Button>
+              </Box>
+            ) : (
+              <Button
+                startIcon={<AddIcon />}
+                variant="text"
+                color="primary"
+                onClick={() => handleExpandNode(uniqueKey)} // Użycie unikalnego klucza
+                style={{ marginTop: "10px" }}
+              >
+                New member
+              </Button>
+            )}
+          </>
+        )}
+
+        {/* Rekurencyjne renderowanie dzieci */}
+        {person.children.length > 0 && (
+          <Box marginLeft="10px">
+            {person.children.map(
+              (child) => renderFamilyTree(child, level + 1) // Wywołanie dla każdego dziecka
+            )}
+          </Box>
+        )}
+      </Box>
+    );
+  };
 
   return (
     <div className="MainPage" style={{ display: "flex" }}>
@@ -240,7 +360,7 @@ function MainPage() {
           <List>
             {familyTrees.map((tree) => (
               <ListItem
-                button
+                button="true"
                 key={tree.id}
                 onClick={() => handleTreeSelect(tree)}
               >
@@ -255,17 +375,24 @@ function MainPage() {
         {/* Wyświetlanie drzewa genealogicznego */}
         {selectedTree && !editingTree && (
           <div>
-            <h2>{selectedTree.treeName}</h2>
-            {/* Jeśli kliknięto już istniejące drzewo, wyświetlamy jego strukturę */}
+            <h2 style={{ display: "flex", alignItems: "center" }}>
+              {selectedTree.treeName}
+              <EditIcon
+                onClick={() => handleEditTree(selectedTree)}
+                style={{
+                  marginLeft: "10px",
+                  cursor: "pointer",
+                  color: "#1976d2", // Kolor ikony
+                }}
+              />
+            </h2>
             {renderFamilyTree(selectedTree)}
-            {console.log("wybrano drzewo", selectedTree)}
           </div>
         )}
 
         {/* Edycja nowego drzewa genealogicznego */}
         {editingTree && (
           <div>
-            {/* Pola założyciela drzewa */}
             <Box marginBottom="20px">
               <TextField
                 size="small"
@@ -288,12 +415,12 @@ function MainPage() {
                   <TextField
                     size="small"
                     sx={{ width: 130 }}
-                    label="Founder first Name"
-                    value={editingTree.firstName}
+                    label="Founder name"
+                    value={editingTree.name}
                     onChange={(e) =>
                       setEditingTree({
                         ...editingTree,
-                        firstName: e.target.value,
+                        name: e.target.value,
                       })
                     }
                     style={{ marginRight: "10px" }}
@@ -301,12 +428,38 @@ function MainPage() {
                   <TextField
                     size="small"
                     sx={{ width: 130 }}
-                    label="Founder last Name"
-                    value={editingTree.lastName}
+                    label="Founder surname"
+                    value={editingTree.surname}
                     onChange={(e) =>
                       setEditingTree({
                         ...editingTree,
-                        lastName: e.target.value,
+                        surname: e.target.value,
+                      })
+                    }
+                    style={{ marginRight: "10px" }}
+                  />
+                  <TextField
+                    size="small"
+                    sx={{ width: 65 }}
+                    label="Birth year"
+                    value={editingTree.birthYear}
+                    onChange={(e) =>
+                      setEditingTree({
+                        ...editingTree,
+                        birthYear: e.target.value,
+                      })
+                    }
+                    style={{ marginRight: "10px" }}
+                  />
+                  <TextField
+                    size="small"
+                    sx={{ width: 65 }}
+                    label="Death year"
+                    value={editingTree.deathYear}
+                    onChange={(e) =>
+                      setEditingTree({
+                        ...editingTree,
+                        deathYear: e.target.value,
                       })
                     }
                     style={{ marginRight: "10px" }}
@@ -316,7 +469,7 @@ function MainPage() {
                     startIcon={<AddIcon />}
                     variant="contained"
                     color="primary"
-                    onClick={() => handleAddFounder()}
+                    onClick={() => setFounderAdded(true)}
                   >
                     Add Founder
                   </Button>
@@ -326,17 +479,14 @@ function MainPage() {
               )}
             </Box>
 
-            {/* Renderowanie drzewa */}
-            {}
-
             {/* Zapis drzewa */}
             <Button
               variant="contained"
               color="secondary"
-              onClick={handleSaveFamilyTree}
+              onClick={updating ? handleUpdateFamilyTree : handleSaveFamilyTree}
               style={{ marginTop: "20px" }}
             >
-              Save Family Tree
+              {updating ? "Update Family Tree" : "Save Family Tree"}
             </Button>
           </div>
         )}
